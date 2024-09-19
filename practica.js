2b8aa11b66f9504a2ca2136e54e07d5358b980be
Javier Polo <jpolo@uc-3-336.AluVedruna.local>
2024-09-19 20:45:10 +0200

    Este es mi trabajo

---
+++ practica.js
@@ -0,0 +1,27 @@
+let array = [3,7,11,8,1,4]
+let edad = prompt("Introduce tu edad")
+
+// 1
+
+for(let i = 0; i<array.length; i++){
+const elemento = array[i];
+console.log(elemento * edad)
+}
+
+// 2
+
+for(let i = 0; i<array.length; i++){
+    const elemento = array[i];
+    console.log(elemento % 2)
+    }
+
+// 3
+
+let string = "Jurado, Alberto, Marcelo, Pablo, Maria"
+
+
+for(let i = 0; i<string.length; i++){
+    const elemento = [i];
+    console.log(elemento % 2)
+    }
+
